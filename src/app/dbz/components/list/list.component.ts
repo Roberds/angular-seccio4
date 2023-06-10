50fa076d4460b6d5ce776f25e3ca1fc2ee7a3c51
roberds <robertodiazgs@gmail.com>
2023-06-10 11:32:58 +0200

    commit seccion 5

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  @Input() //para recibir las propiedades del padre main-component
+  public characterList: Character[] = [];
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+
+
+  onDeleteCharacter(id: string):void {
+    this.onDelete.emit(id);
+  }
+
+}
